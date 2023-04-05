d2fa295e8d12c05170519ddece26ee4a49b6bf93
John Hudson <johnhudson@Johns-MacBook-Pro.local>
2023-04-05 14:46:52 +0100

    add vr button

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 import * as THREE from './node_modules/three/build/three.module.js';
+import { VRButton } from './node_modules/three/examples/jsm/webxr/VRButton.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+
 document.body.appendChild( renderer.domElement );
+document.body.appendChild( VRButton.createButton( renderer ) );
+
+renderer.xr.enabled = true;
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
