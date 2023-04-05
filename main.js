2cacaafd37c4f67c8d62dd4b08bbe6722ec3823c
John Hudson <johnhudson@Johns-MacBook-Pro.local>
2023-04-05 15:21:09 +0100

    change fix render animation

--- main.js
+++ main.js
@@ -13,19 +13,19 @@ document.body.appendChild( VRButton.createButton( renderer ) );
 renderer.xr.enabled = true;
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xcc2000 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
 
-function animate() {
-	setAnimationLoop( animate );
+
+
+renderer.setAnimationLoop( function () {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
-}
 
-animate();
+} );
